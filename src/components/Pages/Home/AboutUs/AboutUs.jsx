5288249b8099e9325d126cb3d3fec01841957a64
nabiursiddique <nabiursiddique01@gmail.com>
2023-07-15 13:28:05 +0600

    Feature- if no product is for sale then page will show and added some little animation

--- src/components/Pages/Home/AboutUs/AboutUs.jsx
+++ src/components/Pages/Home/AboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
     return (
         <div className="hero">
             <div className="hero-content flex-col lg:flex-row">
-                <img src={processor_image} className="lg:w-1/2 rounded-lg shadow-2xl" />
+                <img src={processor_image} className="lg:w-1/2 rounded-lg shadow-2xl animate-fade-right animate-once animate-delay-[1000ms]" />
                 <div className='ml-4'>
                     <h1 className="text-5xl font-bold">About Us</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
--- src/components/Pages/Products/Products/Products.jsx
+++ src/components/Pages/Products/Products/Products.jsx
@@ -23,13 +23,25 @@ const Products = () => {
     }
     return (
         <div>
-            <h1 className='text-3xl text-center my-6 bg-gradient-to-r from-blue-700  to-white text-transparent bg-clip-text font-extrabold'>All Products</h1>
-            <div className='m-6 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 gap-5 my-10'>
+            <div>
+                <h1 className='text-3xl text-center my-6 bg-gradient-to-r from-blue-700  to-white text-transparent bg-clip-text font-extrabold'>All Products</h1>
                 {
-                    products.map((product, ind) => <ProductsCard
-                        key={ind}
-                        product={product}
-                    ></ProductsCard>)
+                    products.length > 0 ?
+                        <div className='m-6 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 gap-5 my-10'>
+                            {
+                                products.map((product, ind) => <ProductsCard
+                                    key={ind}
+                                    product={product}
+                                ></ProductsCard>)
+                            }
+                        </div>
+                        :
+                        <div className='h-[500px] flex justify-center items-center'>
+                            <div className='text-center'>
+                                <h1 className='animate-fade-up animate-once animate-duration-[1500ms] animate-ease-linear text-5xl uppercase font-bold  bg-gradient-to-r from-red-600  to-red-400 text-transparent bg-clip-text'>Sorry</h1>
+                                <h1 className='text-4xl mt-5 uppercase font-bold animate-fade-up animate-once animate-duration-[1500ms] animate-ease-linear  bg-gradient-to-r from-red-600  to-red-400 text-transparent bg-clip-text'> currently no product is availabe!</h1>
+                            </div>
+                        </div>
                 }
             </div>
         </div>
