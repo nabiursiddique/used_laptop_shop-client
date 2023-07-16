aef7f8e139aa5b9f464046bc63c33d8641179cf1
nabiursiddique <nabiursiddique01@gmail.com>
2023-07-16 16:52:12 +0600

    feature- all sellers page is completed.

--- src/components/Pages/Dashboard/AllSellers/AllSellers.jsx
+++ src/components/Pages/Dashboard/AllSellers/AllSellers.jsx
@@ -1,9 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ConfirmationModal from '../../../Shared/ConfirmationModal/ConfirmationModal';
+import { useQuery } from 'react-query';
+import LoadingAnimation from '../../../LittleComponents/LoadingAnimation/LoadingAnimation';
+import { toast } from 'react-hot-toast';
 
 const AllSellers = () => {
+    const [deletingUser, setDeletingUser] = useState(null);
+
+    const closeModal = () => {
+        setDeletingUser(null);
+    }
+
+    // Getting all the user 
+    const { data: sellers = [], isLoading, refetch } = useQuery({
+        queryKey: ["sellers"],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/allSellers?role=Seller');
+                const data = await res.json();
+                return data;
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+    });
+
+    // Delete user from database (This option will be only for admin role)
+    const handleDeleteUser = (seller) => {
+        fetch(`http://localhost:5000/allUsers/${seller._id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.acknowledged) {
+                    toast.success("User deleted successfully.");
+                    refetch();
+                }
+            })
+    }
+
+    if (isLoading) {
+        return <LoadingAnimation></LoadingAnimation>;
+    }
     return (
         <div>
             <h2 className='text-4xl text-center my-5 bg-gradient-to-r from-blue-700  to-white text-transparent bg-clip-text font-extrabold'>All Sellers </h2>
+
+            <div className="overflow-x-auto">
+                <table className="table ">
+                    <thead>
+                        <tr>
+                            <th>Index</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Role</th>
+                            <th>Email</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            sellers.map((seller, ind) =>
+                                <tr key={ind} className="hover">
+                                    <th>{ind + 1}</th>
+                                    <td>
+                                        <div className="avatar">
+                                            <div className="mask mask-circle w-12 h-12">
+                                                <img src={seller.imageURL} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>{seller.name}</td>
+                                    <td>{seller.role}</td>
+                                    <td>{seller.email}</td>
+                                    <th>
+                                        <label htmlFor="confirmation_modal" onClick={() => setDeletingUser(seller)} className="btn bg-red-400 text-white btn-sm hover:bg-red-500">X</label>
+                                    </th>
+                                </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {
+                deletingUser &&
+                <ConfirmationModal
+                    title={`Are you sure you want to delete ${deletingUser.name}?`}
+                    message={`If you delete ${deletingUser.name} it cannot be undone`}
+                    deleteFunction={handleDeleteUser}
+                    successButtonName={"Delete"}
+                    cancelButtonName={"Cancel"}
+                    closeModal={closeModal}
+                    deletingInfo={deletingUser}
+                ></ConfirmationModal>
+            }
         </div>
     );
 };
