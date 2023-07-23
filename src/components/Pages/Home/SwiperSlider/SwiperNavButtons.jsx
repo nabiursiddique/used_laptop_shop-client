d99bda00b5f60b64836e9c9c9768ff5f25f9ddf1
nabiursiddique <nabiursiddique01@gmail.com>
2023-07-23 10:44:22 +0600

    fix- Now in small devices slider arrow will not be visible

--- src/components/Pages/Home/SwiperSlider/SwiperNavButtons.jsx
+++ src/components/Pages/Home/SwiperSlider/SwiperNavButtons.jsx
@@ -4,7 +4,7 @@ import { useSwiper } from 'swiper/react';
 const SwiperNavButtons = () => {
     const swiper = useSwiper()
     return (
-        <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 z-20'>
+        <div className='absolute lg:flex md:flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 z-20 hidden'>
             <button className="btn btn-circle" onClick={()=>swiper.slidePrev()}>❮</button>
             <button className="btn btn-circle" onClick={()=>swiper.slideNext()}>❯</button>
         </div>
